2ccfc8e30d13a280f3148c978616b1b9b06e8138
jpprz7 <perezjohnpatrick077@gmail.com>
2023-04-19 00:27:48 +0800

    rezponzivui

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+/**
+ * Perez, John Patrick A.
+ * BSIT-3F
+ */
+
+/*
 function toggleTheme() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+*/
 
+//pangview ng password
  function showPassword() {
     var x = document.getElementById("password");
     if (x.type === "password") {
@@ -12,3 +20,12 @@ function toggleTheme() {
     }
   }
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function navbarr() {
+  var x = document.getElementById("navbarr");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
